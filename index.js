e8d397cd9fb58df4abb6fefeea85e359439700e6
Hirokazu Chiba <hchiba1@gmail.com>
2022-02-19 20:22:31 +0900

    Add index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const execSync = require('child_process').execSync;
+const chai = require('chai');
+const assert = chai.assert;
+chai.use(require('chai-fs'));
+
+fs.readdirSync('lib').forEach((subdir) => {
+  if (!fs.statSync(`lib/${subdir}`).isDirectory()) {
+    return;
+  }
+  describe(subdir, () => {
+    fs.readdirSync(`lib/${subdir}`).forEach((file) => {
+      if (file.endsWith('.sh')) {
+        const basename = path.basename(file, '.sh');
+        it(file, () => {
+          const result = execSync(`cd lib/${subdir}; ./${basename}.sh`).toString();
+          const expect = fs.readFileSync(`lib/${subdir}/${basename}.txt`).toString();
+          assert.equal(result, expect);
+        });
+      }
+    });
+  });
+});
